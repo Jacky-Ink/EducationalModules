cd08ea73be2996ec3609b59abc2730162e4ead99
Инна Кляхина <inna.galkina95@ya.ru>
2023-04-21 12:06:09 +0300

    v.1.2

--- edu_modules/edu/models.py
+++ edu_modules/edu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class EducatioalModules(models.Model):
+    slug = models.SlugField(
+        null=False,
+        unique=True,
+        verbose_name='Порядковый номер'
+    )
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Название образовательного модуля'
+    )
+    description = models.TextField(
+        verbose_name='Опиасание образовательного модуля'
+    )
+
+    def __str__(self) -> str:
+        return self.name
